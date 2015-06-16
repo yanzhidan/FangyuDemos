b9b9b8d4796b287eb24b2e25b0bcbfb1c52191b8
yanzhidan <hzx861016@163.com>
2015-06-16 17:29:50 +0800

    git for eclipse test

--- src/com/fangyu/demos/TestActivity.java
+++ src/com/fangyu/demos/TestActivity.java
@@ -2,15 +2,14 @@ package com.fangyu.demos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class TestActivity extends Activity
-{
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-	// TODO Auto-generated method stub
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_test);
-
-    }
+public class TestActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_test);
+		Log.e("View", "this is a sample used by yanzhidan");
+	}
 }
